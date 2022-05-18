2e0d7cd494eb30e93158adb8c204374b2196fb24
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-05-18 18:42:27 +0530

    Update GCD_Euclid.java

--- Euclidean GCD/GCD_Euclid.java
+++ Euclidean GCD/GCD_Euclid.java
@@ -17,5 +17,14 @@ public class EuclideanGCD {
 
         }
         return rem;
+        
+        //Beliveing that the value of b is small only. Then the complexity will also be O(log B)
+//        int temp = 0;
+//        while(b != 0){
+//            temp = b;
+//            b = a % b;
+//            a = temp;
+//        }
+//        return a;
     }
 }
